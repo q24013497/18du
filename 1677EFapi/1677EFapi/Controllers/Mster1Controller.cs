7689583c9d25197c6a156d5f429ab629a435e9cf
q24013497 <54700266+q24013497@users.noreply.github.com>
2019-09-09 08:39:29 +0800

    1

---
+++ 1677EFapi/1677EFapi/Controllers/Mster1Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace _1677EFapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Mster1Controller : ControllerBase
+    {
+        Master1BLL bll = new Master1BLL();
+
+        [HttpGet]
+        public List<Master> GetMaster()
+        {
+            List<Master> Masters1 = bll.GetMasters1();
+            return Masters1;
+        }
+    }
+}
